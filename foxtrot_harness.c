c3d9ca9decba0741616aaedd678f47c2e50cefb4
Mark Wutka <mark.wutka@vanderbilt.edu>
2023-04-15 13:30:36 -0500

    Tried some other options for commands

--- foxtrot_harness.c
+++ foxtrot_harness.c
@@ -17,21 +17,40 @@ extern struct nand_device *init_nand_driver(volatile unsigned char *ioregister,
 
 volatile unsigned char ioregister[4] = { 0, 0, 0, 0};
 
+unsigned char read_buff[NUM_BYTES];
+unsigned char write_buff[NUM_BYTES];
 #define NUM_OPS 10
 
 int main()
 {
   const unsigned char addrtmp[1] = { 'a'};
+  unsigned char addr_buff[3] = { 0, 0, 0};
 
   init_nand_driver(ioregister, NULL);
 
+  /*@
+   loop invariant 0 <= i <= NUM_BYTES;
+   loop assigns i, write_buff[0 .. NUM_BYTES-1];
+   loop variant NUM_BYTES-i;
+   */
+  for (int i=0; i < NUM_BYTES; i++) {
+    write_buff[i] = 0;
+  }
+
+  /*@
+   loop invariant 0 <= i <= NUM_BYTES;
+   loop assigns i, read_buff[0 .. NUM_BYTES-1];
+   loop variant NUM_BYTES-i;
+   */
+  for (int i=0; i < NUM_BYTES; i++) {
+    read_buff[i] = 0;
+  }
   struct nand_op_cmd_instr cmd = { 0 };
-  struct nand_op_instr instr = { NAND_OP_CMD_INSTR, { cmd }};
-  cmd.opcode = 0;
-  //@ assert \initialized {Here} (&instr);
+  struct nand_op_instr cmd_instr = { NAND_OP_CMD_INSTR, { cmd, addr, data_in, waitrdy }};
+  //@ assert \initialized {Here} (&cmd_instr);
   struct nand_op_instr instrs[NUM_OPS] = {
-    instr, instr, instr, instr, instr,
-    instr, instr, instr, instr, instr
+    cmd_instr, cmd_instr, cmd_instr, cmd_instr, cmd_instr,
+    cmd_instr, cmd_instr, cmd_instr, cmd_instr, cmd_instr
   };
 
   struct nand_operation op = { instrs, NUM_OPS };
